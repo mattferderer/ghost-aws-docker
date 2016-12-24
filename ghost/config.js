cb0bf47f4f85f77068f3a7696d40bc426ac452af
Matt Ferderer <matt@mattferderer.com>
2016-12-24 10:53:58 -0600

    adjust dev ip in config

--- ghost/config.js
+++ ghost/config.js
@@ -81,10 +81,8 @@ config = {
         // #### Server
         // Can be host & port (default), or socket
         server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            host: '0.0.0.0',
+            port: process.env.GHOST_PORT || '2368'
         },
         // #### Paths
         // Specify where your content directory lives
